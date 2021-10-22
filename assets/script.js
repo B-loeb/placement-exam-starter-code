0ee804c521887f961aeb228e74adb4f501bd2823
Bradley Loeb <bradleyloeb@gmail.com>
2021-10-21 21:06:20 -0400

    everything should work other than the bonus criteria

--- assets/script.js
+++ assets/script.js
@@ -16,14 +16,38 @@ document.getElementById('credit').textContent = `Created by ${yourName}`
 
 document.getElementById('add-gb').addEventListener('click', function () {
     document.getElementById('qty-gb').innerHTML = gb++;
-    document.getElementById('qty-total').innerHTML = total;
-    
+    document.getElementById('qty-total').innerHTML = total++;
+})
+document.getElementById('minus-gb').addEventListener('click', function () {
+    document.getElementById('qty-gb').innerHTML = gb--;
+    document.getElementById('qty-total').innerHTML = total--;
+})
+//Chocolate chip buttons
+document.getElementById('add-cc').addEventListener('click', function () {
+    document.getElementById('qty-cc').innerHTML = cc++;
+    document.getElementById('qty-total').innerHTML = total++;
+})
+document.getElementById('minus-cc').addEventListener('click', function () {
+    document.getElementById('qty-cc').innerHTML = cc--;
+    document.getElementById('qty-total').innerHTML = total--;
+})
+//Sugar buttons
+document.getElementById('add-sugar').addEventListener('click', function () {
+    document.getElementById('qty-sugar').innerHTML = sugar++;
+    document.getElementById('qty-total').innerHTML = total++;
+})
+document.getElementById('minus-sugar').addEventListener('click', function () {
+    document.getElementById('qty-sugar').innerHTML = sugar--;
+    document.getElementById('qty-total').innerHTML = total--;
+})
 
+
+
+ 
 
     // HINT: You can delete this console.log after you no longer need it!
     console.log('Ginger bread + button was clicked!')
 
     // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
-})
 
 // TODO: Hook up event listeners for the rest of the buttons
